0253c438714f00313a2292e1ecc78b01050c8deb
PapierCode <julien@papier-code.fr>
2024-06-03 13:53:54 +0200

    3.0.7 css layout & block editor

--- include/init.php
+++ include/init.php
@@ -1,11 +1,3 @@
 <?php
 
-add_filter( 'pc_filter_news_home_btn_more', 'project_edit_home_btn_more' );
-add_filter( 'pc_filter_events_home_btn_more', 'project_edit_home_btn_more' );
-
-    function project_edit_home_btn_more( $btn_more_args ) {
-
-        $btn_more_args['display'] = true;
-        return $btn_more_args;
-
-    }
+add_filter( 'pc_display_header_inner', '__return_true', 10 );
