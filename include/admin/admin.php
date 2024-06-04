d0484fed4b84976b75b9e3e41805ca1fa32b1227
PapierCode <julien@papier-code.fr>
2024-06-04 09:17:46 +0200

    3.0.10 Editor CSS

--- include/admin/admin.php
+++ include/admin/admin.php
@@ -4,10 +4,10 @@
 =            ACF, filtres valeurs            =
 ===========================================*/
 
-add_filter('acf/load_field/name=bloc_frame_color', 'my_acf_load_field');
+add_filter('acf/load_field/name=bloc_style', 'my_acf_load_field');
 
 	function my_acf_load_field( $field ) {
-		$field['choices']['v1'] = 'Bleu';
+		$field['choices']['v1'] = 'Orange';
 		$field['choices']['v2'] = 'Vert';
 		return $field;
 	}
